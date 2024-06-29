dcfe8614754c3822a11eb6a604acefc764029678
Anya-Phan <nhuanh357@gmail.com>
2024-06-29 13:38:51 +0300

    Making Review Slide

--- assets/js/transition.js
+++ assets/js/transition.js
@@ -1,6 +1,7 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+/* Slide Transition For About */
 const tabUI = $$(".vacation__tab");
 const paneUI = $$(".vacation__pane");
 
@@ -17,6 +18,7 @@ tabUI.forEach((tab, index) => {
     };
 });
 
+/* Not transition - Monthly or Yearly? */
 const listCare = $$(".price__list");
 const isBoxChecked = $("#price__switch-check");
 
@@ -28,4 +30,49 @@ isBoxChecked.onchange = function () {
         listCare[0].style.display = "none";
         listCare[1].style.display = "grid";
     }
+};
+
+/* Slide Transition For Review */
+const listReview = $$(".review__item");
+const listDots = $$(".review__dot");
+const arrowLeft = $(".reviews__wrap-svg-left");
+const arrowRight = $(".reviews__wrap-svg-right");
+
+function nextItem(currentItem, currentIndex) {
+    listDots.forEach((item) => {
+        if (item.classList.contains("active")) {
+            item.classList.remove("active");
+        }
+    });
+    currentItem.classList.add("active");
+    const currentReview = listReview[currentIndex];
+    const review = $(".reviews__list");
+    review.style.left = -1 * currentReview.offsetLeft + "px";
+}
+
+let currentIndex = 0;
+
+//Khi bấm nút dot slide
+listDots.forEach((item, index) => {
+    item.onclick = function () {
+        currentIndex = index;
+        nextItem(item, currentIndex);
+    };
+});
+
+//Khi bấm mũi tên
+arrowLeft.onclick = function () {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = listReview.length - 1;
+    }
+    nextItem(listDots[currentIndex], currentIndex);
+};
+
+arrowRight.onclick = function () {
+    currentIndex++;
+    if (currentIndex > listReview.length - 1) {
+        currentIndex = 0;
+    }
+    nextItem(listDots[currentIndex], currentIndex);
 };
